590aaa85b4abc74e1f88c4949211f0d869b666c9
Ali Yeysides <ali.yeysides@gmail.com>
2023-02-01 00:53:32 -0500

    adds twoSum

---
+++ test.go
@@ -0,0 +1,26 @@
+// package main
+package main
+
+import "fmt"
+
+func twoSum(arr []int, target int) []int {
+	seen := make(map[int]int)
+
+	for i, val := range arr {
+		remainder := target - val
+
+		if _, ok := seen[remainder]; ok {
+			return []int{remainder, i}
+		}
+
+		seen[val] = i
+	}
+	return nil
+}
+
+func main() {
+	arr := []int{3, 4, 4}
+	target := 7
+	res := twoSum(arr, target)
+	fmt.Println(res)
+}

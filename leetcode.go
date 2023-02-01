7bd6cc128b76180ebdc98e185b26ccfc49120645
Ali Yeysides <ali.yeysides@gmail.com>
2023-02-01 16:54:58 -0500

    adds linked lists

--- leetcode.go
+++ leetcode.go
@@ -1,7 +1,10 @@
 // package main
 package main
 
-import "fmt"
+import (
+  "linkedlist" 
+  "fmt"
+)
 
 func twoSum(arr []int, target int) []int {
 	seen := make(map[int]int)
@@ -18,9 +21,41 @@ func twoSum(arr []int, target int) []int {
 	return nil
 }
 
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	carry, dummy := 0, new(ListNode)
+
+	for node := dummy; l1 != nil || l2 != nil || carry > 0; node = node.Next {
+		if l1 != nil {
+			carry += l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			carry += l2.Val
+			l2 = l2.Next
+		}
+
+		node.Next = &ListNode{Val: carry % 10}
+		carry /= 10
+	}
+	return dummy.Next
+}
+
 func main() {
-	arr := []int{3, 4, 4}
-	target := 7
-	res := twoSum(arr, target)
+	// arr := []int{3, 4, 4}
+	// target := 7
+	// res := twoSum(arr, target)
+  l1 := new(ListNode)
+  l2 := new(ListNode)
+
+  Insert(l1, 2)
+  Insert(l1, 4)
+  Insert(l1, 3)
+
+  Insert(l2, 5)
+  Insert(l2, 6)
+  Insert(l2, 4)
+
+  res = addTwoNumbers(l1, l2)
 	fmt.Println(res)
 }
---
+++ vendor/linkedlist/linkedlist.go
@@ -0,0 +1,14 @@
+package linkedlist
+
+// import "fmt"
+
+// ListNode - Definition for singly-linked list.
+type ListNode struct {
+    Val int
+    Next *ListNode
+}
+
+func Insert(node *ListNode, val int) *ListNode {
+  node.Next = &ListNode{Val: val}
+  return node
+}

04c6e8106d1b1a81629f190630dce11740a9634f
Ali Yeysides <ali.yeysides@gmail.com>
2023-05-17 00:11:12 -0400

    adds tests

--- deck/card_test.go
+++ deck/card_test.go
@@ -3,9 +3,12 @@ package deck
 import "fmt"
 
 func ExampleCard() {
-  c := Card{Rank: Ace, Suit: Spade}
-  fmt.Println(c.String())
+  fmt.Println(Card{Rank: Ace, Suit: Spade})
+  fmt.Println(Card{Rank: Nine, Suit: Heart})
+  fmt.Println(Card{Rank: Queen, Suit: Club})
 
   // Output:
   // Ace of Spades
+  // Nine of Hearts
+  // Queen of Clubs
 }

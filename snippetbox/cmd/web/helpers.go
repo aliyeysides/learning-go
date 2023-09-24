18910645ae445f936f3febd6c781406a6e59d685
Ali Yeysides <ali.yeysides@gmail.com>
2023-09-24 19:14:06 -0400

    adds http error handling helpers

---
+++ snippetbox/cmd/web/helpers.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/http"
+)
+
+func (app *Application) serveError(w http.ResponseWriter, r *http.Request, err error) {
+	var (
+		method = r.Method
+		uri    = r.URL.RequestURI()
+	)
+	app.logger.Error(err.Error(), "method", method, "uri", uri)
+	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+}
+
+func (app *Application) clientError(w http.ResponseWriter, status int) {
+	http.Error(w, http.StatusText(status), status)
+}
+
+func (app *Application) notFound(w http.ResponseWriter) {
+	app.clientError(w, http.StatusNotFound)
+}

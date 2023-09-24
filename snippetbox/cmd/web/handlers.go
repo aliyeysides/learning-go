15e0918e145b8f4701460689ff55577436e5ec79
Ali Yeysides <ali.yeysides@gmail.com>
2023-09-24 19:21:28 -0400

    refactor main with helpers

--- snippetbox/cmd/web/handlers.go
+++ snippetbox/cmd/web/handlers.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"log"
 	"log/slog"
 	"net/http"
 	"strconv"
@@ -15,7 +14,7 @@ type Application struct {
 
 func (app *Application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		http.NotFound(w, r)
+    app.notFound(w)
 		return
 	}
 
@@ -27,22 +26,21 @@ func (app *Application) home(w http.ResponseWriter, r *http.Request) {
 
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		log.Print(err.Error())
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+    app.serveError(w, r, err)
 		return
 	}
 
 	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
-		log.Print(err.Error())
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+    app.serveError(w, r, err)
+    return
 	}
 }
 
 func (app *Application) snippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
-		http.NotFound(w, r)
+    app.notFound(w)
 		return
 	}
 
@@ -52,7 +50,7 @@ func (app *Application) snippetView(w http.ResponseWriter, r *http.Request) {
 func (app *Application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
-		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+    app.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
 

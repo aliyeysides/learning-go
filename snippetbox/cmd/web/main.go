01620326397f3654bb2bdfca566e46b245ab7611
Ali Yeysides <ali.yeysides@gmail.com>
2023-09-26 22:09:26 -0400

    adds github.com/jackc/pgx/v5/pgxpool and pg connection pool code

--- snippetbox/cmd/web/main.go
+++ snippetbox/cmd/web/main.go
@@ -1,20 +1,38 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
+
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsn", "postgres://postgres:postgres@localhost:5432/postgres?sslmode=disable", "PostgreSQL data source name")
 	flag.Parse()
 
 	loggerOpts := &slog.HandlerOptions{
 		AddSource: true,
 	}
 	logger := slog.New(slog.NewJSONHandler(os.Stderr, loggerOpts))
+
+	dbpool, err := pgxpool.New(context.Background(), *dsn)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to create connection pool: %v\n", err)
+		os.Exit(1)
+	}
+	defer dbpool.Close()
+
+	if err := dbpool.Ping(context.Background()); err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to ping database: %v\n", err)
+		os.Exit(1)
+	}
+	defer dbpool.Close()
 
 	app := &Application{
 		logger: logger,
@@ -24,7 +42,7 @@ func main() {
 
 	logger.Info("starting server", "addr", *addr)
 
-	err := http.ListenAndServe(*addr, mux)
+	err = http.ListenAndServe(*addr, mux)
 	logger.Error(err.Error())
 	os.Exit(1)
 }
